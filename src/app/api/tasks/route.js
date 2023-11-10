6e8aad65759011ea61432c9135a08f01a2573f03
Carlos D'Lacoste <cdlacostesalazar@gmail.com>
2023-11-09 22:21:31 -0400

    Creados enpoints GET para todas las tareas y POST para crear tareas

--- src/app/api/tasks/route.js
+++ src/app/api/tasks/route.js
@@ -1,5 +1,19 @@
 import { NextResponse } from "next/server";
+import {prisma} from "@/libs/prisma"
 
-export function GET(){
-    return NextResponse.json("creando tareas")
+export async function GET(){
+
+    const tasks = await prisma.task.findMany()
+    return NextResponse.json(tasks)
+}
+
+export async function POST(request, {params}){
+    const {title, description} = await request.json()
+    const newTask = await prisma.task.create({
+        data: {
+            title,
+            description
+        }
+    })
+    return NextResponse.json(newTask)
 }
