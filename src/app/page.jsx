89b8c87a6a9d6a0c05555d3d0cadb3a57a2fffc7
Carlos D'Lacoste <cdlacostesalazar@gmail.com>
2023-11-19 05:08:45 -0400

    Renderizadas tareas de la bd en HomePage

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,3 +1,24 @@
-export default function HomePage(){
-  return <div>Tareas</div>
+import { prisma } from "@/libs/prisma";
+
+async function loadTasks() {
+  return await prisma.task.findMany()
+}
+
+export default async function HomePage(){
+  const tasks = await loadTasks()
+  return (
+    <>
+      <section className="container mx-auto"> 
+        <div className="grid grid-cols-3 gap-3 mt-10">
+          {tasks.map((task) => (
+            <div key={task.id} className="bg-slate-900 p-3 hover:bg-slate-700 hover:cursor-pointer">
+              <h3 className="font-bold text-2xl text-pink-500 mb-2">{task.title}</h3>
+              <p>{task.description}</p>
+              <p>{new Date(task.createdAt).toLocaleDateString()}</p>
+            </div>
+          ))}
+        </div>
+      </section>
+    </>
+  )
 }
