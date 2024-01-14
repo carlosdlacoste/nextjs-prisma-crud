23db8f39543b71a65a3da003a03d8d15d7937b3f
Carlos D'Lacoste <cdlacostesalazar@gmail.com>
2024-01-14 04:48:12 -0400

    Arreglada renderizacion automatica en vercel con la constante dynamic de nextjs

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,6 +5,8 @@ async function loadTasks() {
   return await prisma.task.findMany()
 }
 
+export const dynamic = 'force-dynamic';
+
 export default async function HomePage(){
   const tasks = await loadTasks()
   return (
