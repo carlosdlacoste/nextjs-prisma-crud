24ae3daed7790c1f22165a249c0c16cf0e99f02a
Carlos D'Lacoste <cdlacostesalazar@gmail.com>
2024-01-16 22:02:39 -0400

    Mejorado estilos del navbar y probando list styles en el taskCard

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,18 +5,18 @@ function NavBar(){
         <>
             <nav className="bg-slate-900">
                 <div className="container mx-auto flex justify-between items-center py-3 lg:w-10/12 sm:w-11/12">
-                    <h3 className="font-bold text-3xl">
+                    <h3 className="font-bold text-3xl text-pink-500">
                         NextCrud
                     </h3>
-                    <ul className="flex gap-x-2 text-lg font-bold">
+                    <ul className="flex gap-x-2 text-lg font-bold text-pink-500">
                         <li>
-                            <Link href='/' className="text-slave-300 hover:text-slave-200">Tasks</Link>
+                            <Link href='/' className="hover:text-slate-200">Tasks</Link>
                         </li>
                         <li>
-                            <Link href='/new' className="text-slave-300 hover:text-slave-200">New</Link>
+                            <Link href='/new' className="hover:text-slate-200">New</Link>
                         </li>
                         <li>
-                            <Link href='/about' className="text-slave-300 hover:text-slave-200">About</Link>
+                            <Link href='/about' className="hover:text-slate-200">About</Link>
                         </li>
                     </ul>
                 </div>
--- src/components/taskCard.jsx
+++ src/components/taskCard.jsx
@@ -7,8 +7,11 @@ function TaskCard({task}) {
         <>
             <div className="bg-slate-900 p-3 hover:bg-slate-700 hover:cursor-pointer" onClick={ () => router.push("/tasks/edit/" + task.id)}>
                 <h3 className="font-bold text-2xl text-pink-500 mb-2">{task.title}</h3>
-                <p>{task.description}</p>
-                <p>{new Date(task.createdAt).toLocaleDateString()}</p>
+                <ul className=" ml-4 list-disc">
+
+                    <li>{task.description}</li>
+                    <li>{new Date(task.createdAt).toLocaleDateString()}</li>
+                </ul>
             </div>
         </>
     )
